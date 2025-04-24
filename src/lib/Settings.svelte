c802647d977e0cc0e523004a86cd98cefdb43223
boriselec <boris-elec@mail.ru>
2025-04-24 14:36:30 -0400

    enh: default continuous chat enh: '' -> ""

--- src/lib/Settings.svelte
+++ src/lib/Settings.svelte
@@ -95,7 +95,7 @@ const defaults:ChatSettings = {
   characterName: 'ChatGPT',
   profileName: '',
   profileDescription: '',
-  continuousChat: 'fifo',
+  continuousChat: 'manual-context',
   summaryThreshold: 3000,
   summarySize: 1000,
   summaryExtend: 0,
@@ -104,10 +104,7 @@ const defaults:ChatSettings = {
   pinBottom: 6,
   summaryPrompt: '',
   useSystemPrompt: true,
-  systemPrompt: 'You are an autoregressive language model that has been fine-tuned with instruction-tuning and RLHF. You carefully provide accurate, factual, thoughtful,nuanced answers, and are brilliant at reasoning. If you think there might not be a correct answer, you say so. ' +
-      'Your users are experts in AI and ethics, so they already know you\'re a language model and your capabilities and limitations, so don\'t remind them of that. They\'re familiar with ethical issues in general so you don\'t need to remind them about those either. ' +
-      'Don\'t be verbose in your answers, but do provide details and examples where it might help the explanation. ' +
-      'Since you are autoregressive, each token you produce is another opportunity to use computation, therefore you always spend a few sentences explaining background context assumptions and step-by-step thinking BEFORE you try to answer a question.',
+  systemPrompt: "You are an autoregressive language model that has been fine-tuned with instruction-tuning and RLHF. You carefully provide accurate, factual, thoughtful,nuanced answers, and are brilliant at reasoning. If you think there might not be a correct answer, you say so. Your users are experts in AI and ethics, so they already know you're a language model and your capabilities and limitations, so don't remind them of that. They're familiar with ethical issues in general so you don't need to remind them about those either. Don't be verbose in your answers, but do provide details and examples where it might help the explanation. Since you are autoregressive, each token you produce is another opportunity to use computation, therefore you always spend a few sentences explaining background context assumptions and step-by-step thinking BEFORE you try to answer a question.",
   hideSystemPrompt: false,
   sendSystemPromptLast: false,
   autoStartSession: false,
